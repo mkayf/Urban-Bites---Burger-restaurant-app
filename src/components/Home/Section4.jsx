dfc6d33a0a4cf5f902398a5bfdb7083a61af3b24
mkayf <muhammadkaif150@gmail.com>
2024-08-11 17:24:24 +0500

    food app creation in process

---
+++ src/components/Home/Section4.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Container, Row, Col } from "react-bootstrap";
+import PromotionImage from '../../assets/promotion/pro.png'
+
+const Section4 = () => {
+  return (
+    <>
+    
+    </>
+  )
+}
+
+export default Section4
